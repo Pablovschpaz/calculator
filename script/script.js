665a37f4936899252b913c18b63e9ff69070d654
Pablovschpaz <pablovschpaz@gmail.com>
2020-11-20 13:43:13 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,94 @@
+function numb(x){
+    var display = document.querySelector("#display");
+    display.textContent+=x;
+    display.textContent*=1;
+    if(x=="."){
+        display.textContent+=x;
+    }
+}
+
+function abs(){
+    getValue();
+    display.textContent *= -1;
+}
+
+function sqrt(){
+    getValue();
+    display.textContent = valueDisplay * valueDisplay;
+}
+
+function operator(x){
+    var hist = document.querySelector("#hist");
+    getDisplay();
+
+    if(x=="÷"){
+        hist.textContent=display.textContent;
+        cleanDisplay();
+        document.querySelector("#op").textContent="/";
+    }
+    if(x=="*"){
+        hist.textContent=display.textContent;
+        cleanDisplay();
+        document.querySelector("#op").textContent="*";
+    }
+    if(x=="-"){
+        hist.textContent=display.textContent;
+        cleanDisplay();
+        document.querySelector("#op").textContent="-";
+    }
+    if(x=="+"){
+        hist.textContent=display.textContent;
+        cleanDisplay();
+        document.querySelector("#op").textContent="+";
+    }
+    if(x=="%"){
+        hist.textContent=display.textContent;
+        cleanDisplay();
+        document.querySelector("#op").textContent="%";
+    }
+    
+}
+
+function result(){
+    var valueHist = parseFloat(document.querySelector("#hist").textContent);
+    var valueDisplay = parseFloat(document.querySelector("#display").textContent);
+    let op = document.querySelector("#op").textContent;
+
+    if(valueHist==0){
+        return;
+    }else{
+        if(op=="/"){
+            display.textContent = valueHist / valueDisplay;
+        }
+        if(op=="*"){
+            display.textContent = valueHist * valueDisplay;
+        }
+        if(op=="-"){
+            display.textContent = valueHist - valueDisplay;
+        }
+        if(op=="+"){
+            display.textContent = valueHist + valueDisplay;
+        }
+        if(op=="%"){
+            display.textContent = (valueHist / 100) * valueDisplay;
+        }
+        cleanHist();
+    }
+}
+
+function getDisplay(){
+    display = document.querySelector("#display");
+}
+
+function getValue(){
+    valueHist = parseFloat(document.querySelector("#hist").textContent);
+    valueDisplay = parseFloat(document.querySelector("#display").textContent);
+}
+
+function cleanHist(){
+    hist.textContent = 0;
+}
+
+function cleanDisplay(){
+    display.textContent=0;
+}
